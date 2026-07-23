c206157b3252c46764a46f1c4691bc3df04f3315
Javi <asuncionjavier96@gmail.com>
2026-07-23 23:10:20 +0200

    feat: Añadimos header y footer

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,9 +1,13 @@
 import { CharactersGrid } from "../../component/characterGrid/CharactersGrid";
+import { Footer } from "../../component/Footer/Footer";
+import { Header } from "../../component/Header/Header";
 
 export const HomePage = () =>{
     return(
         <main>
+            <Header />
             <CharactersGrid />
+            <Footer />
         </main>
     );
 };
