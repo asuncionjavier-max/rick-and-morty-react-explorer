928a8d150d470a0aa170aecee950e3d3dbf27004
Javi <asuncionjavier96@gmail.com>
2026-07-23 17:58:27 +0200

    feat: añadimos custom hooks para characters y locations

---
+++ src/hooks/useFetch.jsx
@@ -0,0 +1,30 @@
+import { useEffect, useState } from "react";
+import apiClient from "../api/axios"
+
+
+export const useFetch = (endpoint) =>{
+    const [data, setData] = useState([]);
+    const [loading, setLoading] = useState(false);
+    const [error, setError] = useState(null);
+
+    useEffect (() =>{
+        const fetchData = async () => {
+            try {
+                setLoading (true);
+                setError (null);
+
+                const res = await apiClient.get(endpoint);
+                setData(res.data.results);
+                console.log(res.data.results)
+
+            } catch (err) {
+                console.error(`>Ha habido un problema con ${endpoint}:`, err);
+                setError(err.message || "Ha habido un problema con la API");
+            } finally{
+                setLoading(false);
+            }
+        };
+        fetchData()
+    }, [endpoint]);
+    return {data, loading, error};
+};
